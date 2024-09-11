014bacdfb659c0cb06ab520f0089f1f1a06a9b83
as-Solo <asdelsolo@gmail.com>
2024-09-11 09:33:05 +0200

    deploy

--- js/explosion.js
+++ js/explosion.js
@@ -5,7 +5,7 @@ class Explosion{
         this.top = top;
         this.w = 50;
         this.h = 50;
-        this.path = `./images/explosion/puntas/`
+        this.path = `./images/Explosion/puntas/`
         this.sprite = [`${this.path}ex_01.png`, `${this.path}ex_02.png`, `${this.path}ex_03.png`, `${this.path}ex_04.png`, `${this.path}ex_05.png`, `${this.path}ex_06.png`, `${this.path}ex_07.png`, `${this.path}ex_08.png`, `${this.path}ex_09.png`, `${this.path}ex_10.png`, `${this.path}ex_11.png`, `${this.path}ex_12.png` , `${this.path}ex_13.png` , `${this.path}ex_14.png` , `${this.path}ex_15.png` , `${this.path}ex_16.png` , `${this.path}ex_17.png` , `${this.path}ex_18.png` , `${this.path}ex_19.png`];
     
         this.element =document.createElement("img");
